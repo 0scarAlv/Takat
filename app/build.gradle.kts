836559ef1c6971d481c16e870ebbbbb4e3591fbd
Oscar Alvarado <oscar1arnoldo2@gmail.com>
2026-09-11 20:15:47 -0600

    Release 2.2.2: doble toque en gráfico diario + ocultar montos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.takat.finanzas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 31
-        versionName = "2.2.1"
+        versionCode = 32
+        versionName = "2.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
